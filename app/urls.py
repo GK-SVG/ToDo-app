32fb2272e044990524d5f3a8b5e9bd1b4cf312ac
gk-svg <gk32239@gmail.com>
2020-06-05 13:09:07 +0530

    all authentication urls

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from .import views
 urlpatterns = [
+    path('register/', views.registerPage, name="register"),
+	path('login/', views.loginPage, name="login"),  
+	path('logout/', views.logoutUser, name="logout"),
     path('',views.index,name='Home'),
+    
+
 ]
