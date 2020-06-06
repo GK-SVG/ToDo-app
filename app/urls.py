073fc7b5f65e8061b89e75ca4b7cc1352acedd7a
gk-svg <gk32239@gmail.com>
2020-06-06 20:51:44 +0530

    user action urls added

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,9 @@ urlpatterns = [
     path('register/', views.registerPage, name="register"),
 	path('login/', views.loginPage, name="login"),  
 	path('logout/', views.logoutUser, name="logout"),
+    path('create-work',views.createdata,name='CreateData'),
+    path('getData/',views.getdata,name='GetData'),
+    path('delete/<int:id>',views.deletedata,name='DeleteData'),
     path('',views.index,name='Home'),
-    #path('/create-work')
-
+    
 ]
