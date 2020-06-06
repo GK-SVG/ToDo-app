938d1c8fd4b3f2fcff8f51fafdf99bcad1ae5749
gk-svg <gk32239@gmail.com>
2020-06-06 07:09:36 +0530

    title and user added to models

--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ User = settings.AUTH_USER_MODEL
 # Create your models here.
 class MyData(models.Model):
     user=models.ForeignKey(User, on_delete=models.CASCADE)
+    title=models.CharField(max_length=100,default="")
     data = models.CharField(max_length=250,null=True,blank=True)
     timespan = models.DateTimeField(auto_now_add=True)
 
